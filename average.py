565a50184f3fe3f37dfa40ca5bd5e7f74b1da873
emilywilliamson <71734099+emilywilliamson@users.noreply.github.com>
2020-09-29 18:00:55 -0400

    average python program before installing loop

--- average.py
+++ average.py
@@ -1,11 +1,14 @@
 numberofscores = 0
-score1 = 0
-score2 = 0
+score = 0
 total = 0
 average = 0.0
-scorecount = 0
-#numberofscores = input("please enter the number of scores you want to average: ")
-score1 = int(input("please enter a test score: "))
-score2 = int(input("please enter a test score: "))
-average = (score1 + score2) / 2
+scoreCount = 0
+numberOfScores = int(input("please enter the number of scores you want to average: "))
+
+#What python loop structure could we use to repeat the next 3 lines
+score = int(input("please enter a score: "))
+total = total + score
+scoreCount = scoreCount + 1
+
+average = (total) / numberOfScores
 print(average)
